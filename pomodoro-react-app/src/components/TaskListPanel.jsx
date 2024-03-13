fccc2f5eb45e03e0e93a5d24ae557d26fa938011
James Lawrence <jamdlaw@yahoo.com>
2024-03-13 15:50:47 -0700

    updated code

---
+++ pomodoro-react-app/src/components/TaskListPanel.jsx
@@ -0,0 +1,15 @@
+function TaskListPanel({ tasks }) {
+    return (
+      <div id="taskListPanel" className="task-list-panel">
+        <h2>Completed Tasks</h2>
+        <ul>
+          {tasks.filter(task => task.completed).map((task, index) => (
+            <li key={index}>{`${task.name} - ${task.time} minutes`}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  
+  export default TaskListPanel;
+  
