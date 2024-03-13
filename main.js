b1b5ed0124f523ee801a98257adf4970cc63b7a4
James Lawrence <jamdlaw@yahoo.com>
2024-03-13 15:08:38 -0700

    added comment for example sturucutre

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const timerConfig = {
   remainingTime: { total: 60, minutes: 1, seconds: 0 },
 };
 
+//example { name: <taskName>, time:<minutes> , completed:false }
 const tasks = [];
 const buttonSound = new Audio('button-sound.mp3');
 const modeButtons = document.querySelector('#js-mode-buttons');
