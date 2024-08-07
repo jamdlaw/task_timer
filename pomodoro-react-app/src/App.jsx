d85e85da1814ce5871656c35505180b4e1e41e42
jamdlaw <jamdlaw@yahoo.com>
2024-08-07 16:46:23 -0700

    added clock component

--- pomodoro-react-app/src/App.jsx
+++ pomodoro-react-app/src/App.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Clock from './components/Clock';
 
 function App() {
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
 
   return (
     <div className="app">
-      <h1> Task Timer </h1>        
+      <h1> Task Timer </h1> 
+        <Clock minutes={minutes} seconds={seconds} />
     </div>
   );
 }
