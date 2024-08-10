3698f46578b91029bcf5b02758148b93bfcd6f6e
jamdlaw <jamdlaw@yahoo.com>
2024-08-10 09:07:03 -0700

    abstracting logic out to component

---
+++ pomodoro-react-app/src/components/SetTimer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function SetTimer (taskName, inputMinutes, handleSetTimer, setTaskName) {
+    return (
+        <div className="input-group">
+            <input 
+                type="text" 
+                id="taskName" 
+                name="taskName" 
+                placeholder="Enter task name" 
+                value={taskName} 
+                onChange={(e) => setTaskName(e.target.value)} 
+                className="task-input"
+            />
+            <input 
+                type="number" 
+                value={inputMinutes} 
+                onChange={(e) => setInputMinutes(e.target.value)} 
+                placeholder="Minutes" 
+                className="minutes-input"
+            />
+        <button onClick={handleSetTimer} className="set-timer-button">Set Timer</button>
+        </div>
+     )
+}
+
+export default SetTimer;
