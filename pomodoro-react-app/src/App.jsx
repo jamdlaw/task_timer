b058e5ef91c8631e63ba3969d755352c0ab1a425
jamdlaw <jamdlaw@yahoo.com>
2024-08-10 09:14:33 -0700

    updated app with proper props

--- pomodoro-react-app/src/App.jsx
+++ pomodoro-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from 'react';
 import './App.css';
 import Clock from './components/Clock';
+import SetTimer from './components/SetTimer';
 
 function App() {
   const [minutes, setMinutes] = useState(25);
@@ -65,27 +66,14 @@ function App() {
       <button className="toggle-button" onClick={toggleInputVisibility}>
         {isInputVisible ? 'Hide' : 'Set Timer'}
       </button>
-      {isInputVisible &&
-        <div className="input-group">  
-          <input 
-            type="text" 
-            id="taskName" 
-            name="taskName" 
-            placeholder="Enter task name" 
-            value={taskName} 
-            onChange={(e) => setTaskName(e.target.value)} 
-            className="task-input"
-          />
-            <input 
-              type="number" 
-              value={inputMinutes} 
-              onChange={(e) => setInputMinutes(e.target.value)} 
-              placeholder="Minutes" 
-              className="minutes-input"
-            />
-          <button onClick={handleSetTimer} className="set-timer-button">Set Timer</button>
-      </div>
-    
+      {isInputVisible && 
+        <SetTimer 
+          taskName={taskName}
+          inputMinutes={inputMinutes}
+          handleSetTimer={handleSetTimer}
+          setTaskName={setTaskName}
+          setInputMinutes={setInputMinutes}
+        />
       }
       <div className="buttons-group">
         <button onClick={startTimer}>Start</button>
