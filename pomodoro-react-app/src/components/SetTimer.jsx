679625670987c81893463d2feac1479ab4c7b58f
jamdlaw <jamdlaw@yahoo.com>
2024-08-30 18:37:13 -0700

    markup added to make task name on own line

--- pomodoro-react-app/src/components/SetTimer.jsx
+++ pomodoro-react-app/src/components/SetTimer.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 
-function SetTimer ({taskName, inputMinutes, handleSetTimer, setTaskName, setInputMinutes}) {
-    return (
-        <div className="input-group">
-            <input 
-                type="text" 
-                id="taskName" 
-                name="taskName" 
-                placeholder="Enter task name" 
-                value={taskName} 
-                onChange={(e) => setTaskName(e.target.value)} 
-                className="task-input full-width-input"
-            />
-            <input 
-                type="number" 
-                value={inputMinutes} 
-                onChange={(e) => setInputMinutes(e.target.value)} 
-                placeholder="Minutes" 
-                className="minutes-input"
-            />
-            <button onClick={handleSetTimer} className="set-timer-button">Set Timer</button>
-        </div>
-     )
+function SetTimer({ taskName, inputMinutes, handleSetTimer, setTaskName, setInputMinutes }) {
+  return (
+    <div className="input-group">
+      {/* Separate container for taskName input to make it appear on its own line */}
+      <div className="task-input-container">
+        <input 
+          type="text" 
+          id="taskName" 
+          name="taskName" 
+          placeholder="Enter task name" 
+          value={taskName} 
+          onChange={(e) => setTaskName(e.target.value)} 
+          className="task-input full-width-input"
+        />
+      </div>
+      
+      {/* New line here: Keep inputs for minutes and button together */}
+      <div className="time-input-container">
+        <input 
+          type="number" 
+          value={inputMinutes} 
+          onChange={(e) => setInputMinutes(e.target.value)} 
+          placeholder="Minutes" 
+          className="minutes-input"
+        />
+        <button onClick={handleSetTimer} className="set-timer-button">Set Timer</button>
+      </div>
+    </div>
+  );
 }
 
 export default SetTimer;
